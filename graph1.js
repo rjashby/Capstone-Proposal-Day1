7829ea3e202aa8f2023e23a9e7e807b4d6861995
Ryan Ashby <rjapsu160@gmail.com>
2022-04-29 09:26:16 -0700

    continue youtube course and code along with graphing algorithm

---
+++ graph1.js
@@ -0,0 +1,15 @@
+const airports = "PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM".split(" ");
+
+const routes = [
+  ["PHX", "LAX"],
+  ["PHX", "JFK"],
+  ["JFK", "OKC"],
+  ["JFK", "HEL"],
+  ["JFK", "LOS"],
+  ["MEX", "LAX"],
+  ["MEX", "BKK"],
+  ["MEX", "LIM"],
+  ["MEX", "EZE"],
+  ["LIM", "BKK"],
+];
+
