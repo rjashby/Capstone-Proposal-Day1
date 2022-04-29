df40ba218648157060c6e01e85826fc064b7b08b
Ryan Ashby <rjapsu160@gmail.com>
2022-04-29 10:37:00 -0700

    code DFS and BFS graph traversing methods

--- graph1.js
+++ graph1.js
@@ -34,3 +34,58 @@ airports.forEach(addNode);
 routes.forEach(route => addEdge(...route))
 
 console.log(adjacencyList);
+
+//BFS Breadth First Search
+
+function bfs(start) {
+
+  const visited = new Set();
+
+  const queue = [start];
+
+  while (queue.length > 0) {
+
+    const airport = queue.shift();
+
+    const destinations = adjacencyList.get(airport);
+
+    for (const destination of destinations) {
+
+      if (!visited.has(destination)) {
+        visited.add(destination);
+        queue.push(destination);
+        console.log(destination);
+      }
+
+      if (destination === "BKK") {
+
+        console.log("found it")
+      }
+    }
+  }
+}
+
+// DFS Depth First Search
+
+function dfs(start, visited = new Set()) {
+  
+  console.log(start);
+
+  visited.add(start);
+
+  const destinations = adjacencyList.get(start);
+
+  for (const destination in destinations) {
+  
+    if (destination === "BKK") {
+      
+      console.log("DFS found Bangkok in steps");
+      return;
+    }
+
+    if(!visited.has(destination)) {
+
+      dfs(destination, visited);
+    }
+  }
+}
